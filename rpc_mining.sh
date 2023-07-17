3c658683464421586ba6c73e9e5951c7e7e96a37
khoacung <101453722+khoacung@users.noreply.github.com>
2023-07-17 13:44:06 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyywjapvlv8cqhd4acclueug9t5p2keypepky7kg00xtqjkug3cuqqq7j0r3a -opmem -m 3 -r1 community-pools.mysrv.cloud:10300 -r 146.190.88.29:10100 -r1 nodent2.cpumining.cloud:10100 -p rpc;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xctdaz6xyds3whqq882cl -m 3 -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
